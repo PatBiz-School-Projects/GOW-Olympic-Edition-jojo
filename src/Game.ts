f2bf3a3662fe8ff64b943b9e06e4f22c32162885
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2025-02-26 16:47:53 +0100

    (post-contest) Moved 'no-gravity' version

--- src/Game.ts
+++ src/Game.ts
@@ -1,237 +1,237 @@
-import {
-    Scene,
-    Engine,
-    Vector3,
-    CubeTexture,
-    HemisphericLight,
-    SceneLoader,
-    ActionManager,
-    ExecuteCodeAction,
-    ArcRotateCamera,
-    FreeCamera,
-    Animation,
-    Sound
-  } from "@babylonjs/core";
-  import "@babylonjs/loaders";
-  
-  export class Game {
-    scene: Scene;
-    engine: Engine;
-    camera!: FreeCamera;
-    playerCamera!: ArcRotateCamera;
-  
-    constructor(private canvas: HTMLCanvasElement) {
-      this.engine = new Engine(this.canvas, true);
+//# Third-party :
+import { Engine, Scene, SceneLoader } from "@babylonjs/core";
+import { ArcRotateCamera, FreeCamera, Vector3 } from "@babylonjs/core";
+import { Animation, ActionManager, ExecuteCodeAction } from "@babylonjs/core";
+import type { AbstractMesh } from "@babylonjs/core";
 
-      this.engine.displayLoadingUI();
+import '@babylonjs/loaders';
 
-      this.scene = this.CreateScene();
+//# Local :
+import { Player } from "./player.ts";
+import { Game_SceneBuilder } from "./Game/scene_builder.ts";
+import { Game_PlayerInitialiser } from "./Game/player_initialiser.ts";
 
-      this.CreateCharacter();
 
-      this.CreateEnvironment();
+type GameCameras = {[keys: string]: FreeCamera|ArcRotateCamera};
+type MapMeshes = {[keys: string]: AbstractMesh[]};
 
-      this.CreateCutscene();
-  
-      this.engine.runRenderLoop(() => {
-        
-        this.scene.render();
-      });
-    }
-  
-    CreateScene(): Scene {
-      const scene = new Scene(this.engine);
 
-      this.camera = new FreeCamera("camera", new Vector3(-100, 220, -80), this.scene);
-      this.camera.minZ = 0.5;
-      this.camera.speed = 0.5;
-      this.camera.rotation._y = Math.PI;
+////////////////////////////////////////////////////////////////////////////////
+// Game :
+////////////////////////////////////////////////////////////////////////////////
 
-      const envTex = CubeTexture.CreateFromPrefilteredData(
-        "../assets/environment/environment.env",
-        scene
-      );
-  
-      scene.environmentTexture = envTex;
-  
-      scene.createDefaultSkybox(envTex, true);
-  
-      scene.environmentIntensity = 0.5;
+export class Game {
+    private static _Inst : Game;
 
-      const hemiLight = new HemisphericLight(
-        "hemiLight",
-        new Vector3(0, 1, 0),
-        this.scene
-      );
-  
-      hemiLight.intensity = 0.75;
+    private _canvas : HTMLCanvasElement;
+    private _engine : Engine;
+    private _scene  : Scene;
+    private _player : Player;
 
-      return scene;
+    private _cameras   : GameCameras;
+    private _mapMeshes : MapMeshes;
+
+
+    private constructor() {
+        this._canvas = document.getElementById('renderCanvas')! as HTMLCanvasElement;
+        this._engine = new Engine(this._canvas, true);
+        this._scene  = new Scene(this._engine);
+
+        this._player = new Player(this._scene);
+        this._cameras = {};
+        this._mapMeshes = {};
     }
 
-    async CreateEnvironment(): Promise<void> {
-      await SceneLoader.ImportMeshAsync("", "../assets/models/", "map.glb");
-      
-      const backgroundMusic = new Sound(
-        "backgroundMusic",
-        "./assets/audio/olympics.mp3",
-        this.scene,
-        null,
-        {
-          volume: 0,
-          autoplay: true,
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Getters :
+
+    private static get Inst(): Game {
+        if (!Game._Inst)
+          Game._Inst = new Game();
+        return Game._Inst;
+    }
+
+    // I made the following getters static bcs typing 'Game.Inst.<attribute>'
+    // each time you need to access an attribute of the game is annoying.
+    // Instead, typing 'Game.<attribute>' seem better.
+    public static get canvas()   : HTMLCanvasElement {return Game.Inst._canvas;}
+    public static get engine()   : Engine {return Game.Inst._engine;}
+    public static get scene()    : Scene  {return Game.Inst._scene;}
+    public static get player()   : Player {return Game.Inst._player;}
+    public static get cameras()  : GameCameras {return Game.Inst._cameras;}
+    public static get mapMeshes(): MapMeshes   {return Game.Inst._mapMeshes;}
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Initialisation :
+
+    private initCameras() {
+        /*********************************************/
+        /* Cinematic Camera :                        */
+        /*********************************************/
+
+        const cinematic_cam = new FreeCamera(
+            "CinematicCamera", new Vector3(-100, 220, -80), this._scene
+        );
+        cinematic_cam.minZ = 0.5;
+        cinematic_cam.speed = 0.5;
+        cinematic_cam.rotation._y = Math.PI;
+
+        this._cameras["CinematicCamera"] = cinematic_cam;
+
+        /*********************************************/
+        /* Player Camera :                           */
+        /*********************************************/
+
+        // Constants to fix camera position
+        const arcAlpha = 1.5046653238154635;
+        const arcBeta = 1.5296884191499718;
+        const arcRadius = 44.649908575154164;
+
+        const player_cam = new ArcRotateCamera(
+            "PlayerCamera", arcAlpha, arcBeta, arcRadius, new Vector3(0, 0, 0), this._scene
+        );
+        //player_cam.attachControl();
+        player_cam.minZ = 0.5;
+        player_cam.speed = 0.1;
+        player_cam.wheelPrecision = 10;
+
+        this._cameras["PlayerCamera"] = player_cam;
+    }
+
+    private async initMap() {
+        this._mapMeshes["Decors"] = (await SceneLoader.ImportMeshAsync(
+            "", "../../assets/models/Map/", "decors.glb"
+        )).meshes;
+
+        this._mapMeshes["Obstacles"] = (await SceneLoader.ImportMeshAsync(
+            "", "../../assets/models/Map/", "obstacles.glb"
+        )).meshes;
+
+        this._mapMeshes["SlipperyObjects"] = (await SceneLoader.ImportMeshAsync(
+            "", "../../assets/models/Map/", "slippery-objects.glb"
+        )).meshes;
+
+        this._mapMeshes["GrippableObjects"] = (await SceneLoader.ImportMeshAsync(
+            "", "../../assets/models/Map/", "grippable-objects.glb"
+        )).meshes;
+
+        this._mapMeshes["End"] = (await SceneLoader.ImportMeshAsync(
+            "", "../../assets/models/Map/", "end.glb"
+        )).meshes;
+    }
+
+    private static initPlayerActions() {
+        let key_pressed: "ArrowUp"|"ArrowDown"|"ArrowLeft"|"ArrowRight"|null = null;
+        let shiftPressed: boolean = false
+
+        Game.scene.actionManager = new ActionManager(Game.scene);
+        Game.scene.actionManager.registerAction(new ExecuteCodeAction(
+            ActionManager.OnKeyDownTrigger,
+            (event) => {
+                console.log(event.sourceEvent.key);
+                let key = event.sourceEvent.key;
+                if (key == "Shift") {
+                    shiftPressed = true;
+                    return;
+                }
+                key_pressed = key;
+            }
+        ));
+        Game.scene.actionManager.registerAction(new ExecuteCodeAction(
+            ActionManager.OnKeyUpTrigger,
+            (event) => {
+                let key = event.sourceEvent.key;
+                if (key == "Shift") {
+                    shiftPressed = false;
+                    return;
+                }
+                key_pressed = null;
+            },
+        ))
+
+        const switchMoves = (key: string, shiftPressed: boolean) => {
+            switch (key) {
+                case "ArrowUp"   : Game.player.anim.climbUp(); break;
+                case "ArrowDown" : Game.player.anim.climbDown(); break;
+                case "ArrowLeft" : Game.player.anim.moveLeft(shiftPressed); break;
+                case "ArrowRight": Game.player.anim.moveRight(shiftPressed); break;
+            }
         }
-      );
-  
-      backgroundMusic.setVolume(0.75, 30);
 
-      this.engine.hideLoadingUI();
+        let isMoving = true;
+        Game.scene.onBeforeRenderObservable.add(() => {
+            if(key_pressed) {
+                isMoving = true
+                switchMoves(key_pressed, shiftPressed);
+            } else if (isMoving) {
+                isMoving = false
+                Game.player.anim.hang();
+            }
+        });
     }
 
-  
-    async CreateCharacter(): Promise<void> {
-
-      const arcAlpha = 1.5046653238154635 + (1.6172777427591525 - 1.159866067925365);
-      const arcBeta = 1.5296884191499718 + (1.2714331445491482 - 1.2535226809366065);
-      const arcRadius = 44.649908575154164 + (47.23366662045099 - 44.649908575154164);
-
-      this.playerCamera = new ArcRotateCamera("arcRotateCamera",arcAlpha,  arcBeta, arcRadius, new Vector3(0, 0, 0), this.scene);
-     
-
-      const model = await SceneLoader.ImportMeshAsync(
-        "",
-        "../assets/models/",
-        "climber.glb",
-        this.scene
-      );
-  
-      let player = model.meshes[0];
-      player.scaling.setAll(5);
-      player.position.x = -20;
-      player.position.z = 1;
-
-      this.playerCamera.setTarget(player);
-
-      const hang = this.scene.getAnimationGroupByName("1_hang")!;
-      const hangBraced = this.scene.getAnimationGroupByName("2_hangBraced")!;
-      const bracedHang = this.scene.getAnimationGroupByName("3_bracedHang")!;
-      const climbUp = this.scene.getAnimationGroupByName("4_climbingUp")!;
-      const climbDown = this.scene.getAnimationGroupByName("5_climbingDown")!;
-      const shimmyRight = this.scene.getAnimationGroupByName("6_shimmyRight")!;
-      const shimmyLeft = this.scene.getAnimationGroupByName("7_shimmyLeft")!;
-      const hopRight = this.scene.getAnimationGroupByName("8_hopRight")!;
-      const hopLeft = this.scene.getAnimationGroupByName("9_hopLeft")!;
-
-      
-
-      let keyStatus: { [key: string]: boolean } = {
-        ArrowUp: false,
-        ArrowDown: false,
-        ArrowLeft: false,
-        ArrowRight: false,
-        Shift: false
-    };
-
-      this.scene.actionManager = new ActionManager(this.scene);
-
-      this.scene.actionManager.registerAction( new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (event) => {
-        let key = event.sourceEvent.key;
-        if (key in keyStatus) {
-          keyStatus[key] = true;
-        }
-      }));
-
-      this.scene.actionManager.registerAction( new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (event) => {
-        let key = event.sourceEvent.key;
-        if (key in keyStatus) {
-          keyStatus[key] = false;
-        }
-      }))
-
-      let moving = false;
-      let activeAnim;
-      let animSpeed = 0.1;
-
-      this.scene.onBeforeRenderObservable.add(() => {
-        if(keyStatus.ArrowUp || keyStatus.ArrowDown || keyStatus.ArrowLeft || keyStatus.ArrowRight) {
-          moving = true;
-          if(keyStatus.ArrowUp && !(keyStatus.ArrowDown || keyStatus.ArrowLeft || keyStatus.ArrowRight)) {
-            climbUp.start(true, 1, climbUp.from, climbUp.to, false);
-            player.moveWithCollisions(player.up.scaleInPlace(0.2));
-          }
-          if(keyStatus.ArrowDown && !(keyStatus.ArrowUp || keyStatus.ArrowLeft || keyStatus.ArrowRight)) {
-            climbDown.start(true, 1, hang.from, climbDown.to, false);
-            player.moveWithCollisions(player.up.scaleInPlace(-0.2));
-          }
-          if(keyStatus.ArrowRight && !(keyStatus.ArrowUp || keyStatus.ArrowDown || keyStatus.ArrowLeft)) {
-            activeAnim = keyStatus.Shift ? hopLeft : shimmyLeft;
-            animSpeed = keyStatus.Shift ? 0.2 : 0.1;
-            activeAnim.start(false, 1, activeAnim.from, activeAnim.to, false);
-            player.moveWithCollisions(player.right.scaleInPlace(animSpeed));
-          }
-          if(keyStatus.ArrowLeft && !(keyStatus.ArrowUp || keyStatus.ArrowDown || keyStatus.ArrowRight)) {
-            activeAnim = keyStatus.Shift ? hopRight : shimmyRight;
-            animSpeed = keyStatus.Shift ? 0.2 : 0.1;
-            activeAnim.start(true, 1, activeAnim.from, activeAnim.to, false);
-            player.moveWithCollisions(player.right.scaleInPlace(-animSpeed));
-          }
-        }
-        else if(moving) {
-          hang.start(true, 1, hang.from, hang.to, true);
-          climbUp.stop();
-          climbDown.stop();
-          shimmyLeft.stop();
-          shimmyRight.stop();
-          hopRight.stop();
-          hopLeft.stop();
-          bracedHang.stop();
-          hangBraced.stop();
-          moving = false;
-        }
-      });
-
+    public static async buildScene() {
+        const scene_builder = new Game_SceneBuilder(Game.scene);
+        scene_builder.exec();
+        await Game.Inst.initMap();
+        Game.Inst.initCameras();
     }
 
-    async CreateCutscene(): Promise<void> {
-      const camKeys = [];
-      const fps = 60;
-      const camAnim = new Animation(
-        "camAnim",
-        "position",
-        fps,
-        Animation.ANIMATIONTYPE_VECTOR3,
-        Animation.ANIMATIONLOOPMODE_CONSTANT,
-        true
-      );
-
-
-      camKeys.push({ frame: 0, value: new Vector3(-100, 220, -80) });
-      camKeys.push({ frame: 5 * fps, value: new Vector3(-55, 170, 45) });
-      camKeys.push({ frame: 5 * fps, value: new Vector3(-75, 170, 30) });
-      camKeys.push({ frame: 7 * fps, value: new Vector3(-75, 130, 40) });
-      camKeys.push({ frame: 10 * fps, value: new Vector3(-15, 115, 50) });
-      camKeys.push({ frame: 10 * fps, value: new Vector3(5, 110, 30) });
-      camKeys.push({ frame: 12 * fps, value: new Vector3(13, 75, 0) });
-      camKeys.push({ frame: 13 * fps, value: new Vector3(13, 75, 0) });
-      camKeys.push({ frame: 15 * fps, value: new Vector3(-7, 100, 30) });
-      camKeys.push({ frame: 17 * fps, value: new Vector3(-40, 70, 60) });
-      camKeys.push({ frame: 20 * fps, value: new Vector3(-32, 150, 120) });
-      camKeys.push({ frame: 21 * fps, value: new Vector3(-32, 150, 120) });
-      camKeys.push({ frame: 25 * fps, value: new Vector3(-32, 35, 120) });
-      camKeys.push({ frame: 26 * fps, value: new Vector3(-32, 35, 120) });
-  
-      camAnim.setKeys(camKeys);
-  
-      this.camera.animations.push(camAnim);
-
-  
-      await this.scene.beginAnimation(this.camera, 0, 26 * fps).waitAsync();
-
-      console.log(camAnim);
-      this.EndCutscene();
+    public static async initPlayer() {
+        const player_initialiser = new Game_PlayerInitialiser(
+            Game.scene, Game.player
+        );
+        Game.player.importPlayerModel().then(() => {
+            player_initialiser.exec();
+            Game.player.setAnim();
+            Game.initPlayerActions();
+        });
     }
-  
-    EndCutscene(): void {
-      this.scene.activeCamera = this.playerCamera;
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Actions :
+
+    public static async playCutscene() {
+        Game.scene.activeCamera = Game.cameras["CinematicCamera"];
+
+        const cam = Game.cameras["CinematicCamera"];
+        const fps = 60;
+        const camAnim = new Animation(
+            "camAnim",
+            "position",
+            fps,
+            Animation.ANIMATIONTYPE_VECTOR3,
+            Animation.ANIMATIONLOOPMODE_CONSTANT,
+            true
+        );
+
+        camAnim.setKeys([
+            { frame: 0, value: new Vector3(-100, 220, -80) },
+            { frame: 3 * fps, value: new Vector3(-55, 170, 45) },
+            { frame: 3 * fps, value: new Vector3(-75, 170, 30) },
+            { frame: 5 * fps, value: new Vector3(-75, 130, 40) },
+            { frame: 6 * fps, value: new Vector3(-15, 115, 50) },
+            { frame: 6 * fps, value: new Vector3(5, 110, 30) },
+            { frame: 7 * fps, value: new Vector3(13, 75, 0) },
+            { frame: 8 * fps, value: new Vector3(13, 75, 0) },
+            { frame: 8 * fps, value: new Vector3(-7, 100, 30) },
+            { frame: 10 * fps, value: new Vector3(-40, 70, 60) },
+            { frame: 10 * fps, value: new Vector3(-32, 150, 120) },
+            { frame: 11 * fps, value: new Vector3(-32, 150, 120) },
+            { frame: 15 * fps, value: new Vector3(-32, 35, 120) },
+            { frame: 16 * fps, value: new Vector3(-32, 35, 120) }
+        ]);
+
+        cam.animations.push(camAnim);
+        await this.scene.beginAnimation(cam, 0, 16 * fps).waitAsync();
+
+        Game.scene.activeCamera = Game.cameras["PlayerCamera"];
     }
-  }
+
+    public static play() {
+        Game.engine.runRenderLoop(() => Game.scene.render());
+    }
+}
---
+++ src/Game/scene_builder.ts
@@ -0,0 +1,57 @@
+//# Third-party :
+import type { Scene } from "@babylonjs/core";
+import {
+  Vector3, HemisphericLight, CubeTexture, CannonJSPlugin, Sound
+} from "@babylonjs/core";
+import '@babylonjs/loaders';
+import * as CANNON from "cannon";
+
+
+//-----------------------------------------------------------------------------------
+
+
+export class Game_SceneBuilder {
+    public constructor(
+        private scene: Scene
+    ){}
+
+
+
+  public exec() {
+      const envTex = CubeTexture.CreateFromPrefilteredData(
+          "../../assets/environment/environment.env",
+          this.scene,
+      );
+
+      this.scene.environmentTexture = envTex; // ?????
+      this.scene.createDefaultSkybox(envTex, true);
+
+      const backgroundMusic = new Sound(
+          "backgroundMusic",
+          "./assets/audio/olympics.mp3",
+          this.scene,
+          null,
+          {
+            volume: 0,
+            autoplay: true,
+          }
+      );
+
+      backgroundMusic.setVolume(0.75, 30);
+
+      const hemiLight = new HemisphericLight(
+          "hemiLight",
+          new Vector3(0, 1, 0),
+          this.scene,
+      );
+
+      hemiLight.intensity = 0.75;
+
+      this.scene.environmentIntensity = 0.5;
+
+      this.scene.enablePhysics(
+          new Vector3(0, -9.81, 0),
+          new CannonJSPlugin(true, 10, CANNON)
+      );
+  }
+}
